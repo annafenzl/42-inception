b2d69a0778acab9094d70e86ad3914c6081f969e
Anna Fenzl <annafenzl@mymac.local>
2023-03-27 13:03:57 +0200

    nginx setup done for now, maybe add to conf for wordpress

--- srcs/requirements/nginx/stop_and_remove_images.sh
+++
@@ -1,9 +0,0 @@
-# !/bin/bash
-
-for var in "$@"
-do
-	docker stop "$var"
-	docker rmi -f "$var"
-done
-
-docker images
---
+++ srcs/requirements/nginx/tools/nginx_start.sh
@@ -0,0 +1,17 @@
+#!bin/bash
+
+# setting up ssl
+echo "setting up ssl"
+openssl req -x509 -nodes -days 356 -newkey rsa:4096 -keyout /etc/ssl/private/nginx.key -out /etc/ssl/certs/nginx.crt -subj /C=DE/ST=Heilbronn/L=Heilbronn/O=wordpress/CN=afenzl.42.fr
+echo "ssl is set up!"
+
+exec "$@"
+
+# req": Creates a new X.509 certificate request.
+# "-x509": Generates a self-signed SSL certificate instead of a certificate request.
+# "-nodes": Tells OpenSSL to create a private key without encrypting it with a passphrase.
+# "-days 365": Sets the expiration time of the certificate to 365 days.
+# "-newkey rsa:4096": Generates a new RSA private key with a length of 4096 bits.
+# "-keyout /etc/ssl/private/nginx.key": Sets the path and filename for the private key file.
+# "-out /etc/ssl/certs/nginx.crt": Sets the path and filename for the certificate file.
+# "-subj "/C=RU/ST=Moscow/L=Moscow/O=wordpress/CN=crendeha.42.fr"": Sets the subject field of the certificate with the specified values.
