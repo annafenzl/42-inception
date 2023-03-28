febf78b1a2701c74b8b433fa834d3b76882dbec6
Anna Fenzl <annafenzl@mymac.local>
2023-03-28 14:41:00 +0200

    mariadb set up

--- srcs/requirements/mariadb/tools/mariadb_setup.sql
+++ srcs/requirements/mariadb/tools/mariadb_setup.sql
@@ -1,6 +1,6 @@
-
 CREATE DATABASE IF NOT EXISTS wordpress;
 CREATE USER IF NOT EXISTS 'afenzl'@'%' IDENTIFIED BY 'letmein';
+UPDATE user SET plugin='mysql_native_password' WHERE User='afenzl';
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'root12345';
 GRANT ALL PRIVILEGES ON wordpress.* TO 'afenzl'@'%';
 FLUSH PRIVILEGES; 
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'root12345';
--- srcs/requirements/wordpress/tools/wordpress_setup.sh
+++ srcs/requirements/wordpress/tools/wordpress_setup.sh
@@ -1,39 +1,55 @@
 #!bin/bash
 
-# wp core download --allow-root;
-# echo "Wordpress: creating users..."
+mkdir my-website
+cd my-website
+
+wp core download --allow-root
+# dbhost: ifconfig inet, maybe set back to mariadb when doing docker-compose
+wp core config  --allow-root --dbhost=172.17.0.1:3306 --dbname=wordpress --dbuser=afenzl --dbpass=letmein
+# chmod 600 wp-config.php
+# echo "came until here"
+# wp core install  --allow-root --url=afenzl.42.fr --title="Hello there" --admin_name=ruler --admin_password=letmein --admin_email=annaselinafenzl@gmail.com
+# echo "Wordpress: creating users..."wp core install --url=afenzl.42.fr --title="Hello there" --admin_name=ruler --admin_password=letmein --admin_email=you@example.com
 # # wp --allow-root user create admin admin@example.com --role=administrator --user_pass=<password>
 # # wp --allow-root user create john john@example.com --role=author --user_pass=<password>
 # echo "Wordpress: set up!"
 
-if [ -f ./wp-config.php ]
-then
-	echo "wordpress already downloaded!"
-else
-	echo "setting up wordpress"
+# if [ -f ./wp-config.php ]
+# then
+# 	echo "wordpress already downloaded!"
+# else
+# 	echo "setting up wordpress"
+	
+# 	mkdir wp_setup;
+# 	cd wk_setup;
+# 	wp core download --allow-root;
 
-	#Download wordpress and all config file
-	wget http://wordpress.org/latest.tar.gz
-	tar xfz latest.tar.gz
-	mv wordpress/* .
-	rm -rf latest.tar.gz
-	rm -rf wordpress
 
-	#Inport env variables in the config file
-	# sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
-	# sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
-	# sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
-	# sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
-	# cp wp-config-sample.php wp-config.php
+# 	bash
+# 	# #Download wordpress and all config file
+# 	# wget http://wordpress.org/latest.tar.gz
+# 	# tar xfz latest.tar.gz
+# 	# mv wordpress/* .
+# 	# rm -rf latest.tar.gz
+# 	# rm -rf wordpress
 
-	#Inport env variables in the config file
-	sed -i "s/username_here/afenzl/g" wp-config-sample.php
-	sed -i "s/password_here/letmein/g" wp-config-sample.php
-	sed -i "s/localhost/mariadb/g" wp-config-sample.php
-	sed -i "s/database_name_here/wordpress/g" wp-config-sample.php
-	cp wp-config-sample.php wp-config.php
+# 	#Inport env variables in the config file
+# 	# sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
+# 	# sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
+# 	# sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
+# 	# sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
+# 	# cp wp-config-sample.php wp-config.php
 
-	echo "set up!"
-fi
+# 	#Inport env variables in the config file
+# 	# sed -i "s/database_name_here/wordpress/g" wp-config-sample.php
+# 	# sed -i "s/username_here/afenzl/g" wp-config-sample.php
+# 	# sed -i "s/password_here/letmein/g" wp-config-sample.php
+# 	# sed -i "s/localhost/mariadb/g" wp-config-sample.php
+# 	# cp wp-config-sample.php wp-config.php
+
+# 	# echo "set up!"
+# fi
+
+
 
 exec "$@"
